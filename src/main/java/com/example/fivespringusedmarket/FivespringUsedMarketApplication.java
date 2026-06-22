663d1fba2a7ee6facd6efc08277dce46f45354cf
gpekd5 <gpekd5@naver.com>
2026-06-22 19:32:01 +0900

    chore: 초기 프로젝트 구조 설정

---
+++ src/main/java/com/example/fivespringusedmarket/FivespringUsedMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.fivespringusedmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivespringUsedMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FivespringUsedMarketApplication.class, args);
+    }
+
+}
