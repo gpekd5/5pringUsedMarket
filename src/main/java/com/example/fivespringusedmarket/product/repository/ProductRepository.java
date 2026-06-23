1d80f294fefd05ff4eaa104d3cdd1e6e6bacf69f
hjh <hjh690@naver.com>
2026-06-23 22:40:04 +0900

    feat : 상품 갯수 카운트 쿼리 추가

--- src/main/java/com/example/fivespringusedmarket/product/repository/ProductRepository.java
+++ src/main/java/com/example/fivespringusedmarket/product/repository/ProductRepository.java
@@ -5,7 +5,6 @@ import com.example.fivespringusedmarket.product.entity.ProductCategory;
 import com.example.fivespringusedmarket.product.entity.ProductStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -41,4 +40,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             @Param("deletedStatus") ProductStatus deletedStatus,
             Pageable pageable
     );
+
+    long countBySellerIdAndStatusNot(Long sellerId, ProductStatus status);
 }
