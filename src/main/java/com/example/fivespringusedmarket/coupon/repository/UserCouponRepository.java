e5bbb9debdd759b8996708c62c41f5d92c6a3f6d
hjh <hjh690@naver.com>
2026-06-24 20:04:23 +0900

    feat : 본인 쿠폰 검증 쿼리 추가

--- src/main/java/com/example/fivespringusedmarket/coupon/repository/UserCouponRepository.java
+++ src/main/java/com/example/fivespringusedmarket/coupon/repository/UserCouponRepository.java
@@ -1,6 +1,7 @@
 package com.example.fivespringusedmarket.coupon.repository;
 
 import com.example.fivespringusedmarket.coupon.entity.UserCoupon;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
 
     boolean existsByMemberIdAndCouponId(Long memberId, Long couponId);
+
+    Optional<UserCoupon> findByIdAndMemberId(Long id, Long memberId);
 
     Page<UserCoupon> findByMemberId(Long memberId, Pageable pageable);
 
