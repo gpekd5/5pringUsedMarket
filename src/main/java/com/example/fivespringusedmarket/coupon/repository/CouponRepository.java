f6288ec594045317e293fc42027cd289d21bbeee
hjh <hjh690@naver.com>
2026-06-24 15:38:31 +0900

    feat : CouponRespository 생성

---
+++ src/main/java/com/example/fivespringusedmarket/coupon/repository/CouponRepository.java
@@ -0,0 +1,7 @@
+package com.example.fivespringusedmarket.coupon.repository;
+
+import com.example.fivespringusedmarket.coupon.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+}
