cc9bfd572ba2a7ac14f610368167b3b61d0fd986
hjh <hjh690@naver.com>
2026-06-24 15:37:57 +0900

    feat : Coupon 엔티티 생성

---
+++ src/main/java/com/example/fivespringusedmarket/coupon/entity/Coupon.java
@@ -0,0 +1,49 @@
+package com.example.fivespringusedmarket.coupon.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Entity
+@Table(name = "coupons")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @Column(nullable = false)
+    private int totalQty;
+
+    @Column(nullable = false)
+    private int issuedQty;
+
+    @Column(nullable = false)
+    private LocalDateTime eventStartAt;
+
+    @Column(nullable = false)
+    private LocalDateTime eventEndAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expireAt;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
