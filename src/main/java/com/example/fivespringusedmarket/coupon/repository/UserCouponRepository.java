c448aa4ccee95283be1267025aabddc5a34ed59f
hjh <hjh690@naver.com>
2026-06-24 17:56:34 +0900

    feat : 사용여부에 따른 조회 쿼리 추가

--- src/main/java/com/example/fivespringusedmarket/coupon/repository/UserCouponRepository.java
+++ src/main/java/com/example/fivespringusedmarket/coupon/repository/UserCouponRepository.java
@@ -1,9 +1,17 @@
 package com.example.fivespringusedmarket.coupon.repository;
 
 import com.example.fivespringusedmarket.coupon.entity.UserCoupon;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
 
     boolean existsByMemberIdAndCouponId(Long memberId, Long couponId);
+
+    Page<UserCoupon> findByMemberId(Long memberId, Pageable pageable);
+
+    Page<UserCoupon> findByMemberIdAndUsedAtIsNull(Long memberId, Pageable pageable);
+
+    Page<UserCoupon> findByMemberIdAndUsedAtIsNotNull(Long memberId, Pageable pageable);
 }
