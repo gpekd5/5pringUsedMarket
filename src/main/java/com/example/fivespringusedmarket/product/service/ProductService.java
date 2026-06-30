8fde5f4561c03db90f37826c516631fa6b5c1a86
hjh <hjh690@naver.com>
2026-06-30 12:45:08 +0900

    fix: DELETED 상품 수정 가능 버그 수정

--- src/main/java/com/example/fivespringusedmarket/product/service/ProductService.java
+++ src/main/java/com/example/fivespringusedmarket/product/service/ProductService.java
@@ -68,6 +68,10 @@ public class ProductService {
             throw new CustomException(ErrorCode.ACCESS_DENIED);
         }
 
+		if (product.isDeleted()) {
+			throw new CustomException(ErrorCode.PRODUCT_NOT_FOUND);
+		}
+
         if (product.isSold()) {
             throw new CustomException(ErrorCode.CANNOT_MODIFY_SOLD_PRODUCT);
         }
